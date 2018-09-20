9aaea43cb419e3b6b2eec455dd5301343400b5c6
nutchun <nut.ch40@gmail.com>
2018-09-20 18:55:40 +0700

    Complement with Controller class
    For calculate equations

--- main.py
+++ main.py
@@ -15,12 +15,14 @@
 # 
 
 import PySimpleGUI as sg
+from Controller import *
 
 
 class Calculator:
 
     def __init__(self):
-        self.temp = 0
+        self.temp = "0"
+        self.ctrl = Controller()
     
     def onExec(self):
         """On executing"""
@@ -34,19 +36,45 @@ class Calculator:
         row = 5
 
         form = sg.FlexForm("PySimpleCalculator", auto_size_text=True, auto_size_buttons=False, default_element_size=(40, 1), default_button_element_size=(4, 1), scale=(1, 1), background_color="#222631")
-        layout = [[sg.Text(self.temp, size=(15, 3), font=("Consolas", 30), justification="right", text_color="white", background_color="#222631")]]
+        layout = [[sg.Text(self.temp, key="displayResult", size=(15, 3), font=("Consolas", 30), justification="right", text_color="white", background_color="#222631")]]
         
+        buttonColor = "#333949"
+
+        # draw the buttons
         for i in range(row):
             res = []
             for j in range(col):
-                if buttons[i][j] == "del":
-                    res.append(sg.ReadFormButton(buttons[i][j], image_filename="icon/outline_backspace_white_18dp.png", image_size=(78, 63), image_subsample=1, button_color=("white", "#4a5163"), font=("Consolas", 25)))
+
+                # set bg color for the buttons
+                if buttons[i][j] == "=":
+                    buttonColor = "#33eb91"
+                elif i == 0 or i == row - 1 or j == col - 1:
+                    buttonColor = "#4a5163"
                 else:
-                    res.append(sg.ReadFormButton(buttons[i][j], button_color=("white", "#4a5163"), font=("Consolas", 25)))
+                    buttonColor = "#333949"
+                
+                if buttons[i][j] == "del":
+                    res.append(sg.ReadFormButton(buttons[i][j], image_filename="icon/outline_backspace_white_18dp.png", image_size=(78, 63), image_subsample=1, button_color=("white", buttonColor), font=("Consolas", 25)))
+                else:
+                    res.append(sg.ReadFormButton(buttons[i][j], button_color=("white", buttonColor), font=("Consolas", 25)))
             layout.append(res)
 
-        button, values = form.LayoutAndRead(layout)
-        # sg.Popup(button, values)
+        form.LayoutAndRead(layout)
+
+        while True:
+            button, value = form.Read()
+            if button is None: # close the program
+                break
+            else: # otherwise, pass value to controller
+                self.ctrl.addInput(button)
+                self.temp = self.ctrl.onHandle()
+
+                # check if self.temp is an empty string
+                if not self.temp:
+                    self.temp = "0"
+                
+                # update displayResult text
+                form.FindElement("displayResult").Update(self.temp)
 
 
 if __name__ == "__main__":
